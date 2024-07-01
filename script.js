6564ad33aa00f4db61b81d6c67df68e649f70b00
crystalogy <crystalramos.cmr@gmail.com>
2024-07-01 11:58:49 -0400

    no message

--- script.js
+++ script.js
@@ -112,5 +112,19 @@ class CollapsibleTimeline {
 
       this.animateItemAction(button, ctrld, contentHeight, wasExpanded);
     }
+    // function myFunction() {
+    //   var copyText = document.getElementById("myInput");
+    //   copyText.select();
+    //   copyText.setSelectionRange(0, 99999);
+    //   navigator.clipboard.writeText(copyText.value);
+
+    //   var tooltip = document.getElementById("myTooltip");
+    //   tooltip.innerHTML = "Copied: " + copyText.value;
+    // }
+
+    // function outFunc() {
+    //   var tooltip = document.getElementById("myTooltip");
+    //   tooltip.innerHTML = "Copy to clipboard";
+    // }
   }
 }
